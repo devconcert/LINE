9fac7522ece23aff5e4a6852105a21c43b451150
carpedm20 <carpedm20@gmail.com>
2014-08-02 09:26:11 +0900

    _login in progress

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 LINE
 ====
 
-May the LINE be with you!
+May the LINE be with you...
 
     >>> from line import LineClient
     >>> cli = LineClient(YOUR_ID, YOUR_PASSWORD)
@@ -41,4 +41,4 @@ Source codes are distributed under BSD license.
 Author
 ------
 
-Taehoon Kim / [@carpedm20]('http://carpedm20.github.io/about/')
+Taehoon Kim / [@carpedm20](http://carpedm20.github.io/about/)
--- line/client.py
+++ line/client.py
@@ -7,19 +7,30 @@
     :license: BSD, see LICENSE for more details.
 """
 from __future__ import unicode_literals
+import re
 
 __all__ = ['LineClient']
+
+EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
 class LineClient(object):
     """This class proviede you a way to communicate with LINE server.
 
-    :param email:
+    :param email: NAVER id or NAVER email
+    :param password: LINE password
     """
 
+    __pinCode__ = None
+    __verifier__ = None
+    __provider__ = None
     __certificate__ = None
 
     def __init__(self, email, password):
-        self.email = email
+        if EMAIL_REGEX.match(email):
+            self.__provider__ = "LINE"
+        else:
+            self.__provider__ = "NAVER_KR"
+        self.identifier = email
         self.password = password
 
     def _login(self):
